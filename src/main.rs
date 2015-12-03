f2fe9c4833cd623c301e9daabdc3bc833035e5a9
Connor Taffe <cpaynetaffe@gmail.com>
2015-12-02 21:12:00 -0600

    update

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 
+#![feature(io)]
+
 use std::io;
 use std::io::prelude::*;
 use std::io::BufReader;
 use std::fs::File;
-use std::sync::{Arc, Mutex, MutexGuard};
+use std::sync::{Arc, Mutex };
 use std::sync::mpsc::{ channel, Sender, Receiver };
 use std::thread;
 
@@ -42,6 +44,7 @@ struct Token {
 
 trait Lex<E: std::error::Error> {
     fn next(&self)->Result<char, E>;
+    fn emit(&self);
 }
 
 // State Machine
@@ -52,28 +55,29 @@ struct Lexer<R: io::Read+Send> {
     send: Option<Sender<Token>>
 }
 
-fn startState<E: std::error::Error>(l: &Lex<E>)->Option<StateFn<E>> {
-    unimplemented!()
+fn start_state<E: std::error::Error>(l: &Lex<E>)->Option<StateFn<E>> {
+    l.emit();
+    None
 }
 
-impl<R: io::Read+Send> Lexer<R> {
+impl<R: io::Read+Send+'static> Lexer<R> {
     fn new(reader: R)->Lexer<R> {
         Lexer {
             pos: Pos::new(),
             buf: Arc::new(Mutex::new(reader.chars())),
             state: StateFn {
-                f: startState
+                f: start_state
             },
             send: None
         }
     }
 
-    fn lex(&'static mut self)->(thread::JoinHandle<()>, Receiver<Token>) {
+    fn lex(mut self)->(thread::JoinHandle<()>, Receiver<Token>) {
         let (tx, rx) = channel();
         self.send = Some(tx);
         (thread::spawn(move || {
             loop {
-                match (self.state.f)(self) {
+                match (self.state.f)(&self) {
                     Some(s) => { self.state = s; }
                     None => { return }
                 }
@@ -86,11 +90,19 @@ impl<R: io::Read+Send> Lex<io::CharsError> for Lexer<R> {
     fn next(&self)->Result<char, io::CharsError> {
         self.buf.lock().unwrap().next().unwrap()
     }
+
+    fn emit(&self) {
+        self.send.as_ref().unwrap().send(Token{
+            pos: Pos::new(),
+            typ: TokenType::Integer,
+            val: "Test".to_string()
+        }).unwrap();
+    }
 }
 
-static lexer : &'static mut Lexer<BufReader<File>> = &mut Lexer::new(BufReader::new(File::open("src.f").unwrap()));
-
 fn main() {
+    let lexer = Lexer::new(BufReader::new(File::open("src.f").unwrap()));
     let (th, rx) = lexer.lex();
-    println!("{:?}\n", rx.recv().unwrap());
+    println!("{:?}", rx.recv().unwrap());
+    th.join().unwrap();
 }
